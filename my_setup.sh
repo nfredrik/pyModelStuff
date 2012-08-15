289bf19428941b920209a804c1eb03032b0203bd
fredrik <fredrik@fredrik-VirtualBox.(none)>
2012-08-15 22:56:38 +0200

    set for me

---
+++ my_setup.sh
@@ -0,0 +1,6 @@
+
+export PATH=$PATH:/home/fredrik/pyModelStuff/pymodel
+
+export PYTHONPATH=/home/fredrik/pyModelStuff/samples/$1
+
+#export PYTHONPATH=/home/fredrik/pyModelStuff/samples/PowerSwitch
